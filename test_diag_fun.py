3bddb7c89f54db09fdef324460d07582d06a9bcc
joanna-lewis <joanna.lewis@imperial.ac.uk>
2016-01-19 15:29:52 +0000

    Updated notation in england.ipynb (from SAI to UAS)

--- test_diag_fun.py
+++ test_diag_fun.py
@@ -4,25 +4,25 @@ from numpy import *
 from sympy import *
 
 ##############################
-# function to calculate steady-state A, S and I
+# function to calculate steady-state A, U and S
 ##############################
 
-A, S, I = symbols("A S I")
-alpha_SA, alpha_AS, alpha_SI, alpha_IS  = symbols("alpha_SA alpha_AS alpha_SI alpha_IS")
+A, U, S = symbols("A U S")
+alpha_UA, alpha_AU, alpha_US, alpha_SU  = symbols("alpha_UA alpha_AU alpha_US alpha_SU")
 
 model_dyn = [
-    alpha_SA*S - alpha_AS*A,
-    alpha_AS*A + alpha_IS*I - (alpha_SA + alpha_SI)*S,
-    alpha_SI*S - alpha_IS*I,
-    A + S + I - 1
+    alpha_UA*U - alpha_AU*A,
+    alpha_AU*A + alpha_SU*S - (alpha_UA + alpha_US)*U,
+    alpha_US*U - alpha_SU*S,
+    A + U + S - 1
     ]
 
-sol_dyn = solve(model_dyn, A, S, I)
+sol_dyn = solve(model_dyn, A, U, S)
 
-S_fun = lambdify((alpha_SA, alpha_AS, alpha_SI, alpha_IS), sol_dyn[S])
-A_fun = lambdify((alpha_SA, alpha_AS, alpha_SI, alpha_IS), sol_dyn[A])
-I_fun = lambdify((alpha_SA, alpha_AS, alpha_SI, alpha_IS), sol_dyn[I])
-dyn_fun = lambdify((alpha_SA, alpha_AS, alpha_SI, alpha_IS), sol_dyn[A] + sol_dyn[I])
+U_fun = lambdify((alpha_UA, alpha_AU, alpha_US, alpha_SU), sol_dyn[U])
+A_fun = lambdify((alpha_UA, alpha_AU, alpha_US, alpha_SU), sol_dyn[A])
+S_fun = lambdify((alpha_UA, alpha_AU, alpha_US, alpha_SU), sol_dyn[S])
+dyn_fun = lambdify((alpha_UA, alpha_AU, alpha_US, alpha_SU), sol_dyn[A] + sol_dyn[S])
 
 ##############################
 # now the model for observed testing and diagnosis rates
@@ -31,14 +31,14 @@ dyn_fun = lambdify((alpha_SA, alpha_AS, alpha_SI, alpha_IS), sol_dyn[A] + sol_dy
 tsym, dsym, ssym, test_sym, true_pos, false_pos = symbols("tsym dsym ssym test_sym true_pos false_pos")
 
 model_test_diag = [
-    tsym - ( (A + S)*ssym + (1 - A - S)*test_sym ),
-    dsym - ( A*ssym*true_pos + S*ssym*false_pos + (1 - A - S)*test_sym*true_pos )
+    tsym - ( (A + U)*ssym + (1 - A - U)*test_sym ),
+    dsym - ( A*ssym*true_pos + U*ssym*false_pos + (1 - A - U)*test_sym*true_pos )
     ]
 
-sol_prev = solve(model_test_diag, ssym, S)
+sol_prev = solve(model_test_diag, ssym, U)
 sol_test_diag = solve(model_test_diag, tsym, dsym)
-test_fun = lambdify((A, S, ssym, test_sym, true_pos, false_pos), sol_test_diag[tsym])
-diag_fun = lambdify((A, S, ssym, test_sym, true_pos, false_pos), sol_test_diag[dsym])
+test_fun = lambdify((A, U, ssym, test_sym, true_pos, false_pos), sol_test_diag[tsym])
+diag_fun = lambdify((A, U, ssym, test_sym, true_pos, false_pos), sol_test_diag[dsym])
 
 def test_diag_fun(parms):
     # parms = (incidence, screening rate, proportion symptomatic, self-cure rate, testing rate in symptomatics, true positive rate, false positive rate, ...)
@@ -51,8 +51,8 @@ def test_diag_fun(parms):
     false_pos = parms[6]
     
     A = A_fun(inc*(1-p_symp), self_cure + scr*true_pos, inc*p_symp, self_cure + scr*true_pos + test_sym*true_pos)
-    S = S_fun(inc*(1-p_symp), self_cure + scr*true_pos, inc*p_symp, self_cure + scr*true_pos + test_sym*true_pos)
-    return [test_fun(A, S, scr, test_sym, true_pos, false_pos), diag_fun(A, S, scr, test_sym, true_pos, false_pos)]
+    U = U_fun(inc*(1-p_symp), self_cure + scr*true_pos, inc*p_symp, self_cure + scr*true_pos + test_sym*true_pos)
+    return [test_fun(A, U, scr, test_sym, true_pos, false_pos), diag_fun(A, U, scr, test_sym, true_pos, false_pos)]
     
 ##############################
 # ...and if symptomatic and asymptomatic diagnoses are observed separately:
@@ -61,15 +61,15 @@ def test_diag_fun(parms):
 dssym, dasym = symbols("dssym dasym")
 
 model_test_diag_sa = [
-    tsym - ( (A + S)*ssym + (1 - A - S)*test_sym ),
-    dssym - (1 - A - S)*test_sym*true_pos,
-    dasym - ( A*ssym*true_pos + S*ssym*false_pos )
+    tsym - ( (A + U)*ssym + (1 - A - U)*test_sym ),
+    dssym - (1 - A - U)*test_sym*true_pos,
+    dasym - ( A*ssym*true_pos + U*ssym*false_pos )
     ]
 
 sol_test_diag_sa = solve(model_test_diag_sa, tsym, dssym, dasym)
-test_sa_fun = lambdify((A, S, ssym, test_sym), sol_test_diag_sa[tsym])
-diags_fun = lambdify((A, S, ssym, test_sym, true_pos), sol_test_diag_sa[dssym])
-diaga_fun = lambdify((A, S, ssym, true_pos, false_pos), sol_test_diag_sa[dasym])
+test_sa_fun = lambdify((A, U, ssym, test_sym), sol_test_diag_sa[tsym])
+diags_fun = lambdify((A, U, ssym, test_sym, true_pos), sol_test_diag_sa[dssym])
+diaga_fun = lambdify((A, U, ssym, true_pos, false_pos), sol_test_diag_sa[dasym])
 
 def test_diag_sym_asym_fun(parms):
     # parms = (incidence, screening rate, self-cure rate, testing rate in symptomatics, true positive rate, false positive rate, ...)
@@ -82,5 +82,5 @@ def test_diag_sym_asym_fun(parms):
     false_pos = parms[6]
     
     A = A_fun(inc*(1-p_symp), self_cure + scr*true_pos, inc*p_symp, self_cure + scr*true_pos + test_sym*true_pos)
-    S = S_fun(inc*(1-p_symp), self_cure + scr*true_pos, inc*p_symp, self_cure + scr*true_pos + test_sym*true_pos)
-    return [test_sa_fun(A, S, scr, test_sym), diags_fun(A, S, scr, test_sym, true_pos), diaga_fun(A, S, scr, true_pos, false_pos)]
+    U = U_fun(inc*(1-p_symp), self_cure + scr*true_pos, inc*p_symp, self_cure + scr*true_pos + test_sym*true_pos)
+    return [test_sa_fun(A, U, scr, test_sym), diags_fun(A, U, scr, test_sym, true_pos), diaga_fun(A, U, scr, true_pos, false_pos)]
